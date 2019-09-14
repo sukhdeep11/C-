2f5967f790a30072feb8741055b37882607eef16
sukhdeep singh <sukhdeep.rayat1@gmail.com>
2019-09-14 12:56:24 +0530

    factorial of number

---
+++ problems/factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int fact(int n)
+{
+
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+
+        return n * fact(n - 1);
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the Number : ";
+    cin >> n;
+    cout << "factorial is " << fact(n);
+    return 0;
+}
